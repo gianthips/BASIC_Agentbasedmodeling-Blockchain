75bf7c4d1fa3601645abb380df49fdb9ab7035be
DS Lee <ds13621@gmail.com>
2021-08-19 03:22:06 +0900

    add some accessories & modify Dockerfile in monitored-geth-client dir

---
+++ docker-geth-network-master/activate-logs.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker container logs -f --tail=5 dockergethnetworkmaster_eth_$1
--- docker-geth-network-master/activate-mining.sh
+++ docker-geth-network-master/activate-mining.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 for((i=1; i<= $1; i++)) 
 do
-	docker exec -it docker-geth-network-master_eth_$i geth attach ipc://root/.ethereum/devchain/geth.ipc
+	docker exec -it dockergethnetworkmaster_eth_$i geth attach ipc://root/.ethereum/devchain/geth.ipc
 done
